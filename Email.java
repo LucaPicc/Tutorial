5b64a8064865e5ca912d699f7e50b6a9660ff5ce
Luca Piccini <luca.piccinini0009@gmail.com>
2019-09-17 21:58:23 -0300

    Verificador de @ echo ;D

---
+++ Email.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Email {
+  private Scanner teclado;
+  private String mail;
+  public Email(){
+    teclado=new Scanner(System.in);
+    System.out.print("Ingrese una dirección de correo: ");
+    mail=teclado.nextLine();
+  }
+  public void verificador(){
+    boolean existe=false;
+    for(int i=0;i<mail.length();i++){
+      if(mail.charAt(i)=='@'){
+        existe=true;
+      }
+    }
+    if(existe==true){
+      System.out.print(mail+" contiene el @ " );
+    }
+    else{
+      System.out.print(mail+" no contiene el @ " );
+    }
+  }
+  public static void main(String[] ar){
+    Email direccion = new Email();
+    direccion.verificador();
+  }
+}
